d721a5719f709dfbfba97b598e3a5144a679af90
anniexzxz <24011521@myrp.edu.sg>
2025-07-17 15:46:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -304,7 +304,7 @@ app.get('/deleteProduct/:id', (req, res) => {
     connection.query('DELETE FROM products WHERE productId = ?', [productId], (error, results) => {
         if (error) {
             // Handle any error that occurs during the database operation
-            console.error("Error deleting product:", error);
+            console.error("error deleting product:", error);
             res.status(500).send('Error deleting product');
         } else {
             // Send a success response
